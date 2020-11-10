ce0b1bbd16ab3c2babbd8f16a49a9f5e63554e02
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-11-09 19:40:56 -0600

    Wireframe added; search bar and video component made.

---
+++ client/my-app/src/components/App/App.js
@@ -0,0 +1,14 @@
+import Video from "../Video/Video";
+import SearchBar from "../SearchBar/SearchBar";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+      <SearchBar />
+      <Video />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/my-app/src/components/Video/Video.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div>
+      <iframe
+        title='YouTube Player'
+        id='ytplayer'
+        type='text/html'
+        width='640'
+        height='360'
+        src='https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com'
+        frameborder='0'></iframe>
+    </div>
+  );
+}
