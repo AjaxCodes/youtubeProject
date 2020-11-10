ac93533aee4f49d5226f6945c878bea534040f80
Aj Steinbacher <adamjsteinbacher@gmail.com>
2020-11-10 00:34:43 -0600

    header created, downloded material ui core and material icons, created header.css as well

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    
+    <div className="app">
+      <Header /> 
+      
+
     </div>
   );
 }
