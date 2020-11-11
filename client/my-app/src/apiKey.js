fb6766add410d66f7f46b59adcd57fa5161c9119
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-11-11 15:47:27 -0600

    Update gitignore file.

---
+++ client/my-app/src/apiKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+  apiKey: "AIzaSyB-TOqHu5AM7wZxSaaSwuJEuZn_vZbeILU",
+};
--- client/my-app/src/components/App/App.js
+++ client/my-app/src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Video from "../Video/Video";
 import SearchBar from "../SearchBar/SearchBar";
 import axios from "axios";
+import apiKey from "../../apiKey";
 
 function App() {
   const [query, searchQuery] = useState([]);
@@ -9,7 +10,7 @@ function App() {
   useEffect(() => {
     async function getQuery() {
       const result = await axios(
-        `https://www.googleapis.com/youtube/v3/search?q=${query}&key=AIzaSyBiKmaon_yOMRfM0DlSB8LTASSnRlDWOXM`
+        `https://www.googleapis.com/youtube/v3/search?q=${query}&key=${apiKey}`
       );
       searchQuery(result.query);
     }
@@ -18,7 +19,18 @@ function App() {
 
   const handleSearch = e => {
     e.preventDefault();
-    console.log(e.target.value);
+    function searchByKeyword() {
+      var results = YouTube.Search.list("id,snippet", {
+        q: "dogs",
+        maxResults: 25,
+      });
+
+      for (var i in results.items) {
+        var item = results.items[i];
+        Logger.log("[%s] Title: %s", item.id.videoId, item.snippet.title);
+      }
+    }
+    searchByKeyword();
   };
 
   const handleSubmit = e => {
