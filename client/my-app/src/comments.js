fe2324c9a14879f87547ec5f8254ad30e80e401c
westmoso <71048247+westmoso@users.noreply.github.com>
2020-11-11 20:26:36 -0500

    changes to code after standup with instructors.

--- client/my-app/src/comments.js
+++ client/my-app/src/comments.js
@@ -1,77 +1,45 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {Button, Icon} from "semantic-ui-react";
 import './comments.css';
+import axios from 'axios';
 
 export function Comments(props) {
+    const [Comments, setComment] = useState("")
+    
+    const handleChange = (e) => {
+        setComment(e)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3000/api/comments/add', {
+            name: "",
+            discription: Comments,
+            category:""
+        })
+        .then(res => {
+            console.log(res, "comment saved!");
+          });
+    }
+
+    
+
   return (
-    <div className='comments-header'>
+    <div >
       <h4>{props.amountComments} Comments</h4>
       <Button basic compact icon labelPosition='left'>
         <Icon name='align left' />
         Sort by
       </Button>
+      <label>
+       Leave a Comment
+        <input type="text"  onClick={onSubmit} />
+      </label>
+      <input type="submit" value="Submit"  onChange={e =>handleChange(e.target.value)}/>
     </div>
   );
 }
 
 
-// import React , {useState}from 'react'
-// import axios from 'axios';
-// import Button from '@material-ui/core/Button';
-// import './comments.css'
-// import Input from '@material-ui/core/FilledInput';
- 
-// const { TextArea } = Input
-// let input = 'test'
-
-// function Comments(props) {
-//     const [Comments, setComment] = useState("")
-    
-//     const handleChange = (e) => {
-//         setComment(e.currentTarget.value)
-//     }
-
-//     const onSubmit = (e) => {
-//         e.preventDefault();
-//     }
-
-    
-//     axios.post('/api/comments/add', {
-//             name: "",
-//             discription:"",
-//             category:""
-//         })
-//         .then(res => {
-//             console.log(res);
-//             console.log(res.data);
-//           });
-      
-//         // .then(response=> {
-//         //     if(response.data.success) {
-//         //         setComment("")
-//         //         props.refreshFunction(response.data.result)
-//         //     } else {
-//         //         alert('Comment did not save.')
-//         //     }
-//         // })
-    
-
-//     return (
-//         <div>
-//             <br />
-//             <p className="comments-header">Comments</p>
-//             <br />
-//             {console.log("testing comments")}
-//             <form style={{ display: 'flex' }} onSubmit={onSubmit}>
-//                 < TextArea onChange={handleChange}
-//                     value={Comments}
-//                     placeholder= {input}
-//                 />
-//                 <br />
-//                 <Button style={{ width: '20%', height: '50px' }} onClick={onSubmit}>Submit</Button>
-//             </form>
-//              </div>
-//     )
-// }
 
 export default Comments
