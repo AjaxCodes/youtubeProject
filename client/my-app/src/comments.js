9d844aa6767265496041060de9867649cfea6b9e
westmoso <71048247+westmoso@users.noreply.github.com>
2020-11-12 18:06:30 -0500

    changed comments submit and change functions.

--- client/my-app/src/comments.js
+++ client/my-app/src/comments.js
@@ -1,45 +1,49 @@
-import React , {useState} from 'react';
-import {Button, Icon} from "semantic-ui-react";
+
+import React, { useState } from 'react';
+
 import './comment.css';
 import axios from 'axios';
+import AddComment from './AddComment'
 
-export function Comments(props) {
-    const [Comments, setComment] = useState("")
-    
-    const handleChange = (e) => {
-        setComment(e)
-    }
+function Comments(props) {
+  const [Comments, setComment] = useState("")
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        axios.post('http://localhost:3000/api/comments/add', {
-            name: "",
-            discription: Comments,
-            category:""
-        })
-        .then(res => {
-            console.log(res, "comment saved!");
-          });
-    }
+  const handleChange = e => {
+    e.preventDefault();
+    console.log("Comments console");
+    // props.onSubmit(Comments);
+  };
 
-    
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/api/comments/add', {
+      name: "",
+      discription: Comments,
+      category: ""
+    })
+      .then(res => {
+        console.log(res, "comment saved!");
+      });
+  }
 
   return (
-    <div >
-      <h4>{props.amountComments} Comments</h4>
-      <Button basic compact icon labelPosition='left'>
-        <Icon name='align left' />
-        Sort by
-      </Button>
-      <label>
-       Leave a Comment
-        <input type="text"  onClick={onSubmit} />
-      </label>
-      <input type="submit" value="Submit"  onChange={e =>handleChange(e.target.value)}/>
+    <div className='comment-card'>
+      <form onSubmit={handleChange}>
+        <input
+          type='text'
+          id='input'
+          onChange={e => setComment(e.target.value)}
+        />
+
+        <label>
+          Leave a Comment
+        <input type="text" onClick={onSubmit} />
+        </label>
+        <AddComment />
+      </form>
     </div>
   );
 }
-
 
 
 export default Comments
