96d3ecbe9eed8654265f6874fb6d7c8d50e15b40
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-11-16 18:55:29 -0600

    Added search and video method into main branch.

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,25 +1,35 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
 
-
-
-
 function App() {
-  return (
-    
-    <div className="app">
-      <Header /> 
-      <div className="app__page">
-      <Sidebar />
-      <RecommendedVideos /> 
+  const [video, setVideo] = useState(null);
 
+  const handleSubmit = searchTerm => {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${process.env.REACT_APP_API_KEY_YT}`
+      )
+      .then(response => {
+        setSelectVideo(response.data.items[i].length);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
+  return (
+    <div className='app'>
+      <Header />
+      <div className='app__page'>
+        <Sidebar />
+        <RecommendedVideos />
+        <Video video={setVideo} />
       </div>
     </div>
   );
 }
-
 
 export default App;
